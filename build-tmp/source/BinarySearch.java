a83ed5a873f8b078c83ea14f4fd3047d84261ff7
unknown <ercheung3@697-334-eomer.sf.k12.internal>
2015-03-04 10:45:41 -0800

    working

--- build-tmp/source/BinarySearch.java
+++ build-tmp/source/BinarySearch.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class BinarySearch extends PApplet {
+
 
 private Item[] store = {
         new Item(184,14),
@@ -23,7 +39,7 @@ private Item[] store = {
 };                             
 public int linearSearch(int catNumToFind)
 {
-    for(int i = 0; i < store.length; i++)
+    for(int i = 0; i < store.length-1 ; i++)
     {
        if(store[i].getCatNum() == catNumToFind)
        {
@@ -114,3 +130,12 @@ public void draw()
 
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "BinarySearch" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
